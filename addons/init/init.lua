876bc02f2197fc678ab2f4e101538e4342a623b1
Johan S <johan.sorman@gmail.com>
2023-05-23 14:33:04 +0200

    Update init.lua
    Added FirstUpper function

--- addons/init/init.lua
+++ addons/init/init.lua
@@ -1,6 +1,6 @@
 _addon.name = 'init'
 _addon.author = 'Selk, modified by Sampi'
-_addon.version = '1.0'
+_addon.version = '1.1'
 
 files = require 'files'
 default = 'init_default'
@@ -9,13 +9,17 @@ windower.register_event('login',function (name)
 char = 'init_%s':format(name:lower())
 charname = '%s':format(name:lower())
 
+function firstToUpper(str)
+    return (str:lower():gsub("^%l", string.upper))
+end
+
 if files.exists('..\\..\\scripts\\%s.txt':format(char)) then
 default = char
 end
 
 windower.send_command('exec %s':format(default))
 coroutine.sleep(15) -- Sleep to allow all system messages to load, so log message will appear in the bottom
-windower.add_to_chat(123,'------- Loaded ('.. charname .. ') init profile -------')
+windower.add_to_chat(123,'------- Loaded ('.. firstToUpper(charname) .. ') init profile -------')
 end)
 
 windower.register_event('logout', function(name)
