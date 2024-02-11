6c44d8c3b0ffccb49fad2b90a5733b7e322cf98d
johan-sorman <johan.sorman@gmail.com>
2024-02-11 22:19:13 +0100

    Update bufftracker.lua

--- addons/bufftracker/bufftracker.lua
+++ addons/bufftracker/bufftracker.lua
@@ -3,13 +3,19 @@ _addon.author = 'Melucine'
 _addon.version = '1.1'
 _addon.commands = {'Bufftracker', 'bt'}
 
+-----------------------------------------------------------------------------
+-- Import required modules
+-----------------------------------------------------------------------------
+
 local res = require('resources')
 local zones = require('restricted_zones')
-local trackedBuffs = {}
+local buffRecast = require('recast')
 
 -----------------------------------------------------------------------------
 -- Check Active Buff
 -----------------------------------------------------------------------------
+
+local trackedBuffs = {}
 
 function checkAndActivateBuff()
     if #trackedBuffs == 0 then
@@ -17,38 +23,46 @@ function checkAndActivateBuff()
         return
     end
 
-    local currentZoneID = windower.ffxi.get_info().zone
+    while true do
+        local currentZoneID = windower.ffxi.get_info().zone
+        local player = windower.ffxi.get_player()
 
-    for _, zone in pairs(zones) do
-        if currentZoneID == zone.id then
-            windower.add_to_chat(207, "Cannot use buffs in this area: " .. zone.en)
-            return
+        -- Check if the player is in a restricted zone
+        local isRestrictedZone = false
+        for _, zone in pairs(zones) do
+            if currentZoneID == zone.id then
+                windower.add_to_chat(207, "Cannot use buffs in this area: " .. zone.en)
+                isRestrictedZone = true
+                break
+            end
         end
-    end
 
-    local player = windower.ffxi.get_player()
+        if not isRestrictedZone then
+            for _, buffName in ipairs(trackedBuffs) do
+                local buffActive = false
 
-    for _, buffName in ipairs(trackedBuffs) do
-        local buffActive = false
+                if player and player.buffs then
+                    for _, buffId in ipairs(player.buffs) do
+                        local buff = res.buffs[buffId]
+                        if buff and buff.en == buffName then
+                            buffActive = true
+                            break
+                        end
+                    end
 
-        if player and player.buffs then
-            for _, buffId in ipairs(player.buffs) do
-                local buff = res.buffs[buffId]
-                if buff and buff.en == buffName then
-                    buffActive = true
-                    break
+                    if not buffActive then
+                        local command = buffName .. ' <me>'
+                        windower.send_command(command)
+                    end
                 end
-            end
-
-            if not buffActive then
-                local command = buffName .. ' <me>'
-                windower.send_command(command)
+                coroutine.sleep(5)
             end
         end
-        coroutine.sleep(5)
+
+        coroutine.sleep(240)
     end
-    coroutine.sleep(240) 
 end
+
 
 
 -----------------------------------------------------------------------------
@@ -62,7 +76,8 @@ function handleCommand(subcommand, buffName)
             return
         end
         table.insert(trackedBuffs, buffName)
-        windower.add_to_chat(100, 'Added buff to track: ' .. buffName)
+        local capitalizedbuffName = buffName:sub(1, 1):upper() .. buffName:sub(2)
+        windower.add_to_chat(100, capitalizedbuffName .. ' was added successfully.')
     elseif subcommand == 'remove' then
         if not buffName or buffName == '' then
             windower.add_to_chat(207, "Please specify a buff name to remove.")
@@ -87,20 +102,31 @@ function handleCommand(subcommand, buffName)
             end
         end
     elseif subcommand == 'start' then
-        checkAndActivateBuff()
+        if #trackedBuffs == 0 then
+            windower.add_to_chat(207, "No buffs are being tracked.")
+            return
+        else
+            windower.add_to_chat(207, 'Starting ...')
+            checkAndActivateBuff()
+        end
+
+    elseif subcommand == 'stop' then
+        windower.add_to_chat(207, "Stopping ...")
+        trackedBuffs = {}
 
     elseif subcommand == 'debug' then
         local currentZoneID = windower.ffxi.get_info().zone
         local currentZone = zones[currentZoneID]
-    if currentZone then
-        windower.add_to_chat(100, 'Current Zone: ' .. currentZone.en .. ' (ID: '.. currentZoneID .. ')')
-    else
-        windower.add_to_chat(207, 'Current zone not listed in restricted list.')
-    end
+        if currentZone then
+            windower.add_to_chat(100, 'Current Zone: ' .. currentZone.en .. ' (ID: '.. currentZoneID .. ')')
+        else
+            windower.add_to_chat(207, 'Current zone not listed in restricted list.')
+        end
 
     elseif subcommand == 'clear' then
         trackedBuffs = {}
         windower.add_to_chat(100, "Cleared tracked buffs list.")
+        
     elseif subcommand == 'help' then
         windower.add_to_chat(100, "===== Bufftracker =====")
         windower.add_to_chat(100, "Commands:")
@@ -128,7 +154,6 @@ function handleCommand(subcommand, buffName)
 
         windower.add_to_chat(100, "//bt clear")
         windower.add_to_chat(100, "Clear the list")
-        windower.add_to_chat(100, " ")
 
     else
         windower.add_to_chat(207, "Invalid command. See //bt help for commands.")
